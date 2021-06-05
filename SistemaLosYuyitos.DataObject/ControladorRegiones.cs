f136cc3ca581576a28ad7eec3f048a72f3b57d42
otrivique <otrivique@hotmail.com>
2021-06-05 15:47:48 -0400

    finalizado controlador regiones

--- SistemaLosYuyitos.DataObject/ControladorRegiones.cs
+++ SistemaLosYuyitos.DataObject/ControladorRegiones.cs
@@ -14,13 +14,14 @@ namespace SistemaLosYuyitos.Controlador
 	{
         DataAccess.DataAccess da;
 
-        public List<Region> ObtenerRegiones()
+
+        //llenar cbx de region
+        public List<Region> llenarRegiones()
         {
             List<Region> list = new List<Region>();
-            //Region region = new Region();
             using (da = new DataAccess.DataAccess())
             {
-                da.GenerarComando("select id_region, nombre_region from region");
+                da.GenerarComando(@"select id_region, nombre_region from region");
                 IDataReader reader = da.ExecuteReader();
                 while (reader.Read())
                 {
@@ -36,12 +37,13 @@ namespace SistemaLosYuyitos.Controlador
 
         }
 
-        public List<Provincia> ObtenerProvinciasDeRegion(string id_region)
+        //llenar cbx de provincia
+        public List<Provincia> llenarProvincia(int id_region)
         {
             List<Provincia> list = new List<Provincia>();
             using (da = new DataAccess.DataAccess())
             {
-                da.GenerarComando("select id_provincia, nombre_provincia from provincia where id_region = :id_region ");
+                da.GenerarComando(@"select id_provincia, nombre_provincia from provincia where id_region = :id_region ");
                 da.AgregarParametro(":id_region", id_region);
                 IDataReader reader = da.ExecuteReader();
                 while (reader.Read())
@@ -59,12 +61,13 @@ namespace SistemaLosYuyitos.Controlador
 
         }
 
-        public List<Comuna> ObtenerComunasDeProvincia(string id_provincia)
+        //llenar cbx comuna
+        public List<Comuna> llenarComunas(int id_provincia)
         {
             List<Comuna> list = new List<Comuna>();
             using (da = new DataAccess.DataAccess())
             {
-                da.GenerarComando("select id_comuna, nombre_comuna from comuna where id_provincia = :id_provincia");
+                da.GenerarComando(@"select id_comuna, nombre_comuna from comuna where id_provincia = :id_provincia");
                 da.AgregarParametro(":id_provincia", id_provincia);
                 IDataReader reader = da.ExecuteReader();
                 while (reader.Read())
@@ -79,6 +82,63 @@ namespace SistemaLosYuyitos.Controlador
             }
             return list;
 
+        }
+
+
+        //obtener provincia por id de comuna
+        public Provincia obtenerProvincia ( int id_comuna)
+        {
+            Provincia provincia = null;
+            using (da = new DataAccess.DataAccess()) 
+            {
+                da.GenerarComando(@"select p.id_provincia, p.nombre_provincia, p.id_region
+                                    from provincia p
+                                    inner join comuna c on (p.id_comuna = c.id_comuna)
+                                    where c.id_comuna = :id_comuna");
+                da.AgregarParametro(":id_comuna", id_comuna);
+                IDataReader reader = da.ExecuteReader();
+                while (reader.Read())
+                {
+                    provincia = new Provincia
+                    {
+                        IdProvincia = Convert.ToInt32(reader["id_provincia"].ToString()),
+                        NombreProvincia = reader["nombre_provincia"].ToString(),
+                        IdRegion = Convert.ToInt32(reader["id_region"].ToString())
+                    };
+
+                }
+
+
+            }
+            return provincia;
+        }
+
+
+        //obtener region por id de provincia
+        public Region obtenerRegion(int id_provincia)
+        {
+            Region region = null;
+            using (da = new DataAccess.DataAccess())
+            {
+                da.GenerarComando(@"select r.id_region, r.nombre_region
+                                    from region r
+                                    inner join provincia p on (r.id_region = p.id_region)
+                                    where p.id_provincia = :id_provincia");
+                da.AgregarParametro(":id_provincia", id_provincia);
+                IDataReader reader = da.ExecuteReader();
+                while (reader.Read())
+                {
+                    region = new Region
+                    {
+                        IdRegion = Convert.ToInt32(reader["id_region"].ToString()),
+                        NombreRegion = reader["nombre_region"].ToString()
+                    };
+
+                }
+
+
+            }
+            return region;
         }
 
     }
