e4e399998aa84b4def34d27f97eb15c3bb09fca5
otrivique <otrivique@hotmail.com>
2021-06-06 13:54:54 -0400

    producto de la orden

--- SistemaLosYuyitos.Entidad/Pedido.cs
+++ SistemaLosYuyitos.Entidad/Pedido.cs
@@ -8,5 +8,9 @@ namespace SistemaLosYuyitos.Entidad
 {
     public class Pedido
     {
+        public string CodigoBarra { get; set; }
+        public int NumeroOrden { get; set; }
+        public int Cantidad { get; set; }
+        public int SubTotal { get; set; }
     }
 }
